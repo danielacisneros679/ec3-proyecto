57cab754632c23320bf755d08d86f5edb646b106
danielacisneros679 <cisnerospintodaniela@gmail.com>
2025-03-15 17:16:22 -0500

    Agregamos spring y bootstrap

---
+++ src/main/java/pe/edu/idat/examen3_web/Examen3WebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.examen3_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen3WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen3WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/examen3_web/Examen3WebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.examen3_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen3WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
