06c45e7db228d02fd109712bbfb217ba2018ee7b
alonsobox <alonsobox_14@hotmail.com>
2025-03-15 18:46:14 -0500

    Agregamos cambios al HTML

--- src/main/java/pe/edu/idat/examen3_web/Controller/ComisionController.java
+++ src/main/java/pe/edu/idat/examen3_web/Controller/ComisionController.java
@@ -1,4 +1,15 @@
 package pe.edu.idat.examen3_web.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ComisionController {
+
+    @GetMapping("/examen")  // Ahora responde a http://localhost:8080/examen
+    public String mostrarFormulario(Model model) {
+        model.addAttribute("mensaje", "Calculadora de Comisiones");
+        return "examen";  // No pongas ".html"
+    }
 }
