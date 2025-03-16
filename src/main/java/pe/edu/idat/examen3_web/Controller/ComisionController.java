51511a0060a68b5b33dcb0a63be6b6f61dc53b5a
alonsobox <alonsobox_14@hotmail.com>
2025-03-15 19:35:26 -0500

    realizamos el primet formulario

--- src/main/java/pe/edu/idat/examen3_web/Controller/ComisionController.java
+++ src/main/java/pe/edu/idat/examen3_web/Controller/ComisionController.java
@@ -3,13 +3,35 @@ package pe.edu.idat.examen3_web.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.examen3_web.Model.ComisionModel;
 
 @Controller
 public class ComisionController {
 
-    @GetMapping("/examen")  // Ahora responde a http://localhost:8080/examen
+    @GetMapping("/examen")
     public String mostrarFormulario(Model model) {
-        model.addAttribute("mensaje", "Calculadora de Comisiones");
-        return "examen";  // No pongas ".html"
+        model.addAttribute("comisionmodel", new ComisionModel()); // Agregar el objeto
+        return "examen"; // Nombre del archivo en templates/
+    }
+
+    @PostMapping("/calcularComision")
+    public String calcularComision(@ModelAttribute("comisionmodel") ComisionModel comision, Model model) {
+        double ventas = comision.getVentasMensuales();
+        double comisionCalculada;
+
+        if (ventas < 1000) {
+            comisionCalculada = ventas * 0.03;
+        } else if (ventas >= 1000 && ventas <= 5000) {
+            comisionCalculada = ventas * 0.05;
+        } else if (ventas > 5000 && ventas <= 10000) {
+            comisionCalculada = ventas * 0.07;
+        } else {
+            comisionCalculada = ventas * 0.10;
+        }
+
+        model.addAttribute("comisionCalculada", comisionCalculada);
+        return "examen"; // Debe haber un examen.html en templates
     }
 }
--- src/main/java/pe/edu/idat/examen3_web/Model/ComisionModel.java
+++ src/main/java/pe/edu/idat/examen3_web/Model/ComisionModel.java
@@ -1,4 +1,22 @@
 package pe.edu.idat.examen3_web.Model;
 
 public class ComisionModel {
+    private Double ventasMensuales;
+    private Double comision;
+
+    public Double getVentasMensuales() {
+        return ventasMensuales;
+    }
+
+    public void setVentasMensuales(Double ventasMensuales) {
+        this.ventasMensuales = ventasMensuales;
+    }
+
+    public Double getComision() {
+        return comision;
+    }
+
+    public void setComision(Double comision) {
+        this.comision = comision;
+    }
 }
